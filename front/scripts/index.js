aabd63aa49a3f53e58728debc71e9af74c9f5cca
AlanBeisel <eliaas.beisel@gmail.com>
2024-03-06 09:43:24 -0300

    homework2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,44 +1,40 @@
-console.log(tempData);
+const div = document.getElementById("div2");
+
+$.get("https://students-api.2.us-1.fl0.io/movies", (data) => {
+    renderCards(data);
+});
 
 
+const renderCards = (data) => {
+data.map(obj => {
 
-tempData.map(obj => {
+const contenedorPeli = document.createElement("div");
+contenedorPeli.classList.add("movie-card"); 
 
-// Crea un contenedor div para cada película
-const target = document.createElement("div");
-target.classList.add("movie-card"); 
-
-// Crea un elemento img para el póster de la película
-const poster = document.createElement("img"); // Aquí se corrigió el nombre de la variable
+const poster = document.createElement("img"); 
 poster.src = obj.poster;
 
 const movieTitleLink = document.createElement("a");
 movieTitleLink.textContent = obj.title; 
 movieTitleLink.href = "#";
 
-// Crea un elemento h3 para el título de la película
 const movieTitle = document.createElement("h4");
 
 const movieYear = document.createElement("p");
 movieYear.textContent = "Año: " + obj.year; 
 
-// Crea un elemento para el director de la película
 const movieDirector = document.createElement("p");
 movieDirector.textContent = "Director: " + obj.director;
 
- // Añade los elementos al contenedor
-target.appendChild(poster);
-target.appendChild(movieTitle);
-target.appendChild(movieYear);
-target.appendChild(movieDirector);
+contenedorPeli.appendChild(poster);
+contenedorPeli.appendChild(movieTitle);
+contenedorPeli.appendChild(movieYear);
+contenedorPeli.appendChild(movieDirector);
 movieTitle.appendChild(movieTitleLink);
 
-
-
-
-
-const div = document.getElementById("div2");
-
-div.appendChild(target);
+div.appendChild(contenedorPeli);
 });
+
+};
+
 
